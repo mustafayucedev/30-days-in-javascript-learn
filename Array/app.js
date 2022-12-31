3b54ba4268861b7c0c4e2da0514063fd789af718
Mustafa Yüce <75587873+mustafayucedev@users.noreply.github.com>
2022-12-31 18:18:38 +0300

    add array folders

---
+++ Array/app.js
@@ -0,0 +1,53 @@
+// .push();
+
+// .pop();
+
+// .shift();
+
+// .unshift();
+
+// .toString();
+
+// .valueOf();
+
+// .splice();
+
+// .slice();
+
+// .concat();
+
+// .indexOf();
+
+// .lastIndexOf();
+
+// .includes();
+
+// .filter();
+
+// .map();
+
+// .find();
+
+// .findIndex();
+
+// .sort();
+
+// .reverse();
+
+// .split();
+
+// .join();
+
+// .reduce();
+
+// .reduceRight();
+
+// .isArray();
+
+// .fill();
+
+// .some();
+
+// .entries();
+
+// .keys();

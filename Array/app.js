f16ec795fe0334ba4442d72d6c08d49039bc4f87
Mustafa Yüce <75587873+mustafayucedev@users.noreply.github.com>
2023-01-02 17:07:41 +0300

    Update app.js

--- Array/app.js
+++ Array/app.js
@@ -1,14 +1,26 @@
-// .push();
+// .push(); dizinin sonuna belirtilen değeri ekler
+const x = [1,2,3];
+x.push(4);
+console.log(x); // [1,2,3,4]
 
-// .pop();
+// .pop(); dizinin sonuncu değerini siler.
+x.pop();
+console.log(x); // [1,2,3]
 
-// .shift();
+// .unshift(); diziye belirlenen ilk değeri ekler.
+x.unshift(9);
+console.log(x); // [9,1,2,3]
 
-// .unshift();
+// .shift(); dizinin ilk değerini siler
+x.shift();
+console.log(x); // [1,2,3]
 
 // .toString();
 
-// .valueOf();
+// .valueOf(); object olan array'leri primitive haline döndürür.
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+const myFruits = fruits.valueOf();
+document.body.innerHTML = myFruits; // Banana,Orange,Apple,Mango
 
 // .splice();
 
